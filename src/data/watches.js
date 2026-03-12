095d36a980b8005d6a9df7adde8ae57509bae640
Yush Maskey <yushmaskey4@gmail.com>
2026-03-12 08:43:32 +0545

    added data section

---
+++ src/data/watches.js
@@ -0,0 +1,30 @@
+export const WATCHES = [
+  {
+    id: 1,
+    name: "Rolex Submariner",
+    price: 9500,
+    category: "Luxury",
+    image: "https://images.unsplash.com/photo-1547996160-81dfa63595aa?q=80&w=1887"
+  },
+  {
+    id: 2,
+    name: "Omega Speedmaster",
+    price: 6000,
+    category: "Sports",
+    image: "https://images.unsplash.com/photo-1523170335258-f5ed11844a49?q=80&w=1780"
+  },
+  {
+    id: 3,
+    name: "Patek Philippe",
+    price: 45000,
+    category: "Vintage",
+    image: "https://images.unsplash.com/photo-1614164185128-e4ec99c436d7?q=80&w=1887"
+  },
+  {
+    id: 4,
+    name: "Tissot Le Locle",
+    price: 850,
+    category: "Classic",
+    image: "https://images.unsplash.com/photo-1587836374828-4dbaba94ee0e?q=80&w=1740"
+  }
+];
